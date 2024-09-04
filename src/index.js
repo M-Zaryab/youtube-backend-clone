7ed71587e868764770f134a408d89574266a88bb
Zaryab <zaryabimran222@gmail.com>
2024-09-04 22:47:20 +0500

    connection to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./env" });
+
+connectDB();
+
+/* (async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (err) => {
+      console.log("ERROR: ", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App listening on port ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.log("ERROR: ", err);
+    throw err;
+  }
+})();*/
